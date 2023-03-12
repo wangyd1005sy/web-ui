c7fde92c42e19aed19886d2f096735637e3e1a27
dongzai1005 <wangyd1005sy@163.com>
2023-03-12 21:14:39 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <template>
-  <div>
-    <router-view></router-view>
-  </div>
+  <router-view />
 </template>
+
 <script setup>
+import useSettingsStore from '@/store/modules/settings'
+import { handleThemeStyle } from '@/utils/theme'
+
+onMounted(() => {
+  nextTick(() => {
+    // 初始化主题样式
+    handleThemeStyle(useSettingsStore().theme)
+  })
+})
 </script>
-<style scoped>
-</style>
--- src/api/province.js
+++
@@ -1,8 +0,0 @@
-import http from '../utils/http/http.js'
-
-const getProvinceList = (data) => {
-    return http.get("/province/getProvinceList", data);
-};
-export default {
-    getProvinceList
-}
